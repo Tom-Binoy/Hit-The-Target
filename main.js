79deec9e8a19615fc43b54d59ac8dbc5f68c2738
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-01-20 17:11:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,22 @@ function new_image()
 	
 }
 
+function new()
+{	canvas.remove(b_o);
+	// write code to Upload ball image on canvas
+	fabric.Image.fromURL("ball.png",function(Img){
+		b_o =Img;
+		b_o.scaleToWidth(50);
+		b_o.scaleToHeight(50);
+		b_o.set({
+			top : b_y,
+			left : b_x
+		});
+		canvas.add(b_o);
+	});
+	
+}
+
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
@@ -85,10 +101,9 @@ function my_keydown(e)
 	{
 		// Write a code to move ball upward.
 		if(b_y >0){
-			b_y = b_y-block_image_height;
-			console.log("Up arrow key is pressed, Y ="+b_y)
-			canvas.remove(b_o);
-			new_image();
+		 b_y = b_y-block_image_height;
+		 console.log("Up arrow key is pressed, Y ="+b_y)
+		 new();
 		 }
 	}
 
@@ -96,32 +111,29 @@ function my_keydown(e)
 	{
 		 // Write a code to move ball downward.
 		 if(b_y <=450){
-			b_y+=block_image_height;
-			console.log("Down arrow key is pressed, Y ="+b_y)
-			canvas.remove(b_o);
-			new_image();
+		 b_y+=block_image_height;
+		 console.log("Down arrow key is pressed, Y ="+b_y)
+		 new();
 		 }
 	}
 
 	function left()
 	{
-		if(ball_x >5)
+		if(b_x >5)
 		{
-				b_x+=block_image_height;
-				console.log("Left arrow key is pressed, X ="+b_x)
-				canvas.remove(b_o);
-				new_image();
+		b_x-=block_image_height;
+		console.log("Left arrow key is pressed, X ="+b_x)
+		new();
 		}
 	}
 
 	function right()
 	{    
-		if(ball_x < 1050){
-			console.log("r okay");
-			b_x+=block_image_height;
-				console.log("Right arrow key is pressed, X ="+b_x)
-				canvas.remove(b_o);
-				new_image();
+		if(b_x < 1050){
+		console.log("r okay");
+		b_x+=block_image_height;
+		console.log("Right arrow key is pressed, X ="+b_x)
+		new();
 		}
 	}
 	
